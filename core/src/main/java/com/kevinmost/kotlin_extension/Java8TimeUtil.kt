8e150903a3e8174c1f89035aec1c97f12b636aa0
Kevin Most <kevin@kevinmost.com>
2016-04-29 09:44:59 -0400

    Removed Java 8 stuff for now

--- core/src/main/java/com/kevinmost/kotlin_extension/Java8TimeUtil.kt
+++
@@ -1,9 +0,0 @@
-package com.kevinmost.kotlin_extension
-
-import java.time.Instant
-import java.time.LocalDateTime
-import java.time.ZoneId
-
-fun Instant.localDateTime(): LocalDateTime {
-    return LocalDateTime.ofInstant(this, ZoneId.systemDefault())
-}
