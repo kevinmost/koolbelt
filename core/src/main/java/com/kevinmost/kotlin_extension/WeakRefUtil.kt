475c16e53df9e28524f2c47f2595a3345694519a
Kevin Most <kevin@kevinmost.com>
2016-05-01 02:09:22 -0400

    Added WeakReference delegated property

---
+++ core/src/main/java/com/kevinmost/kotlin_extension/WeakRefUtil.kt
@@ -0,0 +1,18 @@
+package com.kevinmost.kotlin_extension
+
+import java.lang.ref.WeakReference
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+fun <T> weakReference(referent: T): ReadOnlyProperty<Any, T?> = Weak(referent)
+
+private class Weak<T>(referent: T): ReadOnlyProperty<Any, T?> {
+
+  private val weakReference = WeakReference(referent)
+
+  override fun getValue(thisRef: Any, property: KProperty<*>): T? {
+    return weakReference.get()
+  }
+
+}
+
