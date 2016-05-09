085b661af99bea37d13db50860ede6b8103e4efd
Kevin Most <kevin@kevinmost.com>
2016-05-09 01:12:04 -0400

    Cleaned up MergingListMapTests

--- core/src/test/java/com/kevinmost/kotlin_toolbelt/util/MergingListMapTest.kt
+++ core/src/test/java/com/kevinmost/kotlin_toolbelt/util/MergingListMapTest.kt
@@ -7,22 +7,31 @@ import org.junit.Test
 class MergingListMapTest {
   @Test fun `test plus-equals pair`() {
     var map = mergingListsMapOf("Foo" to listOf(1))
-    assertEquals(map.size, 1)
-    assertEquals(map["Foo"]?.size, 1)
+    assertEquals(1, map.size)
+    assertEquals(1, map["Foo"]?.size)
 
     map += "Bar" to listOf(2)
-    assertEquals(map.size, 2)
-    assertEquals(map["Foo"]?.size, 1)
-    assertEquals(map["Bar"]?.size, 1)
+    assertEquals(2, map.size)
+    assertEquals(1, map["Foo"]?.size)
+    assertEquals(1, map["Bar"]?.size)
 
     map += "Foo" to listOf(3, 4, 5)
-    assertEquals(map.size, 2)
+    assertEquals(2, map.size)
     assertTrue(map["Foo"]!!.contains(3))
-    assertEquals(map["Foo"]?.size, 4)
+    assertEquals(4, map["Foo"]?.size)
 
     map += "Foo" to listOf(7)
-    assertEquals(map.size, 2)
+    assertEquals(2, map.size)
     assertTrue(map["Foo"]!!.contains(7))
-    assertEquals(map["Foo"]?.size, 5)
+    assertEquals(5, map["Foo"]?.size)
+  }
+
+  @Test fun `test set value doesn't overwrite`() {
+    val map = mergingListsMapOf("Foo" to listOf(1))
+    assertEquals(1, map["Foo"]!!.size)
+
+    map["Foo"] = listOf(2)
+
+    assertEquals(2, map["Foo"]!!.size)
   }
 }
