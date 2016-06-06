de493ec598e42be4e76247fb913fcb67df87357b
Kevin Most <kevin@kevinmost.com>
2016-06-06 13:40:13 -0400

    Optimized splitting Iterable function

--- core/src/main/java/com/kevinmost/kotlin_toolbelt/extension/IterableUtil.kt
+++ core/src/main/java/com/kevinmost/kotlin_toolbelt/extension/IterableUtil.kt
@@ -1,7 +1,52 @@
 package com.kevinmost.kotlin_toolbelt.extension
 
-fun <T> Iterable<T>.splitEvery(n: Int): List<List<T>> {
-  return splitWhen { index, element, currentSubList -> index % n == 0 }
+import java.util.ArrayList
+
+/**
+ * Invokes [block] upon each individual sublist that is chunked out from every [n] elements in this
+ * [Iterable].
+ *
+ * @param [useLeftoversAtEnd] whether or not to use any remaining elements at the end of the list
+ * that do not divide evenly into [n].
+ */
+@JvmOverloads
+inline fun <T> Iterable<T>.every(n: Int, useLeftoversAtEnd: Boolean = true,
+    block: (List<T>) -> Unit) {
+  val currentList = ArrayList<T>(n)
+  for (element in this) {
+    if (currentList.size == n) {
+      block(currentList)
+      currentList.clear()
+    }
+    currentList.add(element)
+  }
+  if (useLeftoversAtEnd && currentList.isNotEmpty()) {
+    block(currentList)
+  }
+}
+
+/**
+ * Collects individual sublists of size [n] from the receiver [Iterable].
+ *
+ * @param [useLeftoversAtEnd] whether or not to use any remaining elements at the end of the list
+ * that do not divide evenly into [n].
+ */
+fun <T> Iterable<T>.splitEvery(n: Int, useLeftoversAtEnd: Boolean = true): List<List<T>> {
+  val result = mutableListOf<MutableList<T>>()
+
+  var currentWorkingList = ArrayList<T>(n)
+  for (element in this) {
+    if (currentWorkingList.size == n) {
+      result.add(currentWorkingList)
+      currentWorkingList = ArrayList<T>(n)
+    }
+    currentWorkingList.add(element)
+  }
+  if (useLeftoversAtEnd && currentWorkingList.isNotEmpty()) {
+    result.add(currentWorkingList)
+  }
+
+  return result
 }
 
 inline fun <T> Iterable<T>.splitWhen(
