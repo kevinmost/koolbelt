4422b1f8a1bdf1967ff158d930b13bf70fecf8fb
Kevin Most <kevin@kevinmost.com>
2016-06-14 10:02:53 -0400

    Improved SelfReference to inline its initializer block

--- core/src/main/java/com/kevinmost/kotlin_toolbelt/util/SelfReference.kt
+++ core/src/main/java/com/kevinmost/kotlin_toolbelt/util/SelfReference.kt
@@ -1,19 +1,20 @@
 package com.kevinmost.kotlin_toolbelt.util
 
-class SelfReference<T>(val initializer: SelfReference<T>.() -> T) {
+class SelfReference<T> {
   val self: T by lazy {
-    inner ?: throw IllegalStateException("Do not use `self` until initialized.")
+    __self ?: throw IllegalStateException("Do not use `self` until initialized.")
   }
 
-  private val inner = initializer()
+  var __self: T? = null
 }
 
 /**
  * Allows referencing "this" (via the "self" variable) while initializing this value
  *
- * See: http://stackoverflow.com/q/35100389 for the idea and a sample.
+ * See: http://stackoverflow.com/q/35100389 for the idea and a sample. Rewritten slightly to
+ * allow the [initializer] block to be inlined.
  */
-fun <T> selfReference(initializer: SelfReference<T>.() -> T): T {
-  return SelfReference(initializer).self
+inline fun <T> selfReference(initializer: SelfReference<T>.() -> T): T {
+  return SelfReference<T>().apply { __self = initializer() }.self
 }
 
