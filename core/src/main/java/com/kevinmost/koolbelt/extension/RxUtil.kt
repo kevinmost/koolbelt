3252d5c16032b779697b895ec174ca6e585a6ed3
Kevin Most <kevin@kevinmost.com>
2016-07-12 15:01:19 -0400

    Add Subject extensions
    Also remove operator overload of Observable + Observable to be
    combineLatest, and made them an infix. This was too presumptuous

--- core/src/main/java/com/kevinmost/koolbelt/extension/RxUtil.kt
+++ core/src/main/java/com/kevinmost/koolbelt/extension/RxUtil.kt
@@ -3,6 +3,10 @@ package com.kevinmost.koolbelt.extension
 import rx.Observable
 import rx.Scheduler
 import rx.schedulers.Schedulers
+import rx.subjects.AsyncSubject
+import rx.subjects.BehaviorSubject
+import rx.subjects.PublishSubject
+import rx.subjects.ReplaySubject
 import java.util.concurrent.Future
 
 @Suppress("UNCHECKED_CAST")
@@ -29,10 +33,27 @@ inline fun <T> Future<T>.onReturn(crossinline doOnReturn: (T) -> Unit) {
   toObservable().subscribe { doOnReturn(it) }
 }
 
-operator fun <T1, T2> Observable<T1>.plus(other: Observable<T2>): Observable<Pair<T1, T2>> {
+infix fun <T1, T2> Observable<T1>.combineWith(other: Observable<T2>): Observable<Pair<T1, T2>> {
   return Observable.combineLatest(this, other) { first, second -> first to second }
 }
 
-fun <T1, T2> Observable<T1>.combineWith(other: Observable<T2>): Observable<Pair<T1, T2>> {
-  return this + other
+infix fun <T1, T2> Observable<T1>.zipWith(other: Observable<T2>): Observable<Pair<T1, T2>> {
+  return this.zipWith(other) { first, second -> first to second }
 }
+
+
+var <T> BehaviorSubject<T>.currentValue: T
+  get() = this.value
+  set(value) = onNext(value)
+
+var <T> PublishSubject<T>.currentValue: T
+  get() = throw UnsupportedOperationException("PublishSubject does not cache its emitted values; there is no concept of a 'current' value to retrieve!")
+  set(value) = onNext(value)
+
+var <T> ReplaySubject<T>.currentValue: T
+  get() = this.value
+  set(value) = onNext(value)
+
+var <T> AsyncSubject<T>.currentValue: T
+  get() = this.value
+  set(value) = onNext(value)
