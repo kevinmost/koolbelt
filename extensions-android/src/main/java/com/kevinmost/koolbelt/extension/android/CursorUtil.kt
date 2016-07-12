c7619ff9d4cb15c1c9ff165b413d22214833242b
Kevin Most <kevinmost@users.noreply.github.com>
2016-07-12 19:30:31 -0400

    Fixed off-by-one in CursorUtil! (#19)

--- extensions-android/src/main/java/com/kevinmost/koolbelt/extension/android/CursorUtil.kt
+++ extensions-android/src/main/java/com/kevinmost/koolbelt/extension/android/CursorUtil.kt
@@ -9,12 +9,29 @@ import android.database.Cursor
  * being consumed. The [consumeFunction] *must* not in any way alter the cursor (such as moving
  * rows or closing it), or this function will fail.
  */
-inline fun <T> Cursor?.consumeAll(consumeFunction: (Cursor) -> T): List<T> {
-  return this?.use {
-    val list = mutableListOf<T>()
-    while (this.moveToNext()) {
-      list.add(consumeFunction(this))
+inline fun <T: Any> Cursor?.consumeAll(crossinline consumeFunction: (Cursor) -> T): List<T> {
+  return toSequence(consumeFunction).toList()
+}
+
+/**
+ * Uses this Cursor until consumed, moving automatically down the rows.
+ *
+ * @param consumeFunction a function that takes in a cursor and returns the result of one row
+ * being consumed. The [consumeFunction] *must* not in any way alter the cursor (such as moving
+ * rows or closing it), or this function will fail.
+ */
+inline fun <T : Any> Cursor?.toSequence(crossinline consumeFunction: (Cursor) -> T): Sequence<T> {
+  if (this == null || !this.moveToFirst()) return emptySequence()
+
+  this.use {
+    return generateSequence {
+      return@generateSequence if (this.isAfterLast) {
+        null
+      } else {
+        consumeFunction(this).apply {
+          this@toSequence.moveToNext()
+        }
+      }
     }
-    return@use list
-  } ?: emptyList()
+  }
 }
