a1e8828573a7a24ba31b7e251f05888aab6cee67
Kevin Most <kevin@kevinmost.com>
2016-07-13 18:41:46 -0400

    Revert to old CursorUtil but with some tweaks
    - Move to the first element before doing anything, and then use a
      do-while loop to move to the next element. This ensures that even if
      the Cursor was provided not on the first element, we get everything
    - Create the output List with the size equal to the Cursor's length

--- extensions-android/src/main/java/com/kevinmost/koolbelt/extension/android/CursorUtil.kt
+++ extensions-android/src/main/java/com/kevinmost/koolbelt/extension/android/CursorUtil.kt
@@ -1,6 +1,7 @@
 package com.kevinmost.koolbelt.extension.android
 
 import android.database.Cursor
+import com.kevinmost.koolbelt.extension.mutableListOfSize
 
 /**
  * Uses this Cursor until consumed, moving automatically down the rows.
@@ -9,31 +10,14 @@ import android.database.Cursor
  * being consumed. The [consumeFunction] *must* not in any way alter the cursor (such as moving
  * rows or closing it), or this function will fail.
  */
-inline fun <T : Any> Cursor?.consumeAll(crossinline consumeFunction: (Cursor) -> T): List<T> {
-  return toSequence(consumeFunction).toList()
+inline fun <T> Cursor?.consumeAll(consumeFunction: (Cursor) -> T): List<T> {
+  return this?.use {
+    val list = mutableListOfSize<T>(count)
+    moveToFirst()
+    do {
+      list.add(consumeFunction(this))
+    } while (this.moveToNext())
+    return@use list
+  } ?: emptyList()
 }
 
-/**
- * Uses this Cursor until consumed, moving automatically down the rows.
- *
- * @param consumeFunction a function that takes in a cursor and returns the result of one row
- * being consumed. The [consumeFunction] *must* not in any way alter the cursor (such as moving
- * rows or closing it), or this function will fail.
- */
-inline fun <T : Any> Cursor?.toSequence(crossinline consumeFunction: (Cursor) -> T): Sequence<T> {
-  if (this == null || !this.moveToFirst()) return emptySequence()
-
-  var moreRowsExist: Boolean = true
-
-  return this.use {
-    generateSequence {
-      if (moreRowsExist) {
-        consumeFunction(this).apply {
-          moreRowsExist = moveToNext()
-        }
-      } else {
-        null
-      }
-    }
-  }
-}
