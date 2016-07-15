c81cc38eebbe293c54ea4a7aef3b1e36841a50e2
Kevin Most <kevinmost@users.noreply.github.com>
2016-07-15 16:41:11 -0400

    Add dp and sp convenience extensions for Context (#35)

--- extensions-android/src/main/java/com/kevinmost/koolbelt/extension/android/ContextUtil.kt
+++ extensions-android/src/main/java/com/kevinmost/koolbelt/extension/android/ContextUtil.kt
@@ -9,3 +9,29 @@ fun Context.getPlural(@PluralsRes id: Int,
     : CharSequence {
   return resources.getQuantityString(id, quantity, *args)
 }
+
+inline fun Context.dimensions(block: DimensionsContext.() -> Unit) {
+  DimensionsContext(this).apply(block)
+}
+
+class DimensionsContext(private val context: Context) {
+
+  val Int.dp: Int
+    get() = (this * density).toInt()
+
+  val Int.sp: Int
+    get() = (this * scaledDensity).toInt()
+
+  val Int.px2Dp: Float
+    get() = (this.toFloat() / density).toFloat()
+
+  val Int.px2Sp: Float
+    get() = (this.toFloat() / scaledDensity).toFloat()
+
+
+  private val density: Float
+    get() = context.resources.displayMetrics.density
+
+  private val scaledDensity: Float
+    get() = context.resources.displayMetrics.scaledDensity
+}
